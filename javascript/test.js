5002fae7082d5627700d24f22bc016aec24b9bcf
ruanyl <ruanyu1@gmail.com>
2014-04-02 19:21:53 +0300

    modification

--- javascript/test.js
+++ javascript/test.js
@@ -17,7 +17,7 @@ function caculater() {
   this.chain = function(o) {
     this.formula.unshift(o);
     if(this.formula.length < 3) {
-      return false;
+      return 'false';
     } else if(this.formula.length === 3) {
       return this.cal();
     }
@@ -26,76 +26,76 @@ function caculater() {
 function zero(c) {
   this.__proto__ = c ? c : new caculater();
   var result = this.chain(0);
-  return result ? result : this.__proto__;
+  return result !== 'false' ? result : this.__proto__;
 }
 function one(c) {
   this.__proto__ = c ? c : new caculater();
   var result = this.chain(1);
-  return result ? result : this.__proto__;
+  return result !== 'false' ? result : this.__proto__;
 }
 function two(c) {
   this.__proto__ = c ? c : new caculater();
   var result = this.chain(2);
-  return result ? result : this.__proto__;
+  return result !== 'false' ? result : this.__proto__;
 }
 function three(c) {
   this.__proto__ = c ? c : new caculater();
   var result = this.chain(3);
-  return result ? result : this.__proto__;
+  return result !== 'false' ? result : this.__proto__;
 }
 function four(c) {
   this.__proto__ = c ? c : new caculater();
   var result = this.chain(4);
-  return result ? result : this.__proto__;
+  return result !== 'false' ? result : this.__proto__;
 }
 function five(c) {
   this.__proto__ = c ? c : new caculater();
   var result = this.chain(5);
-  return result ? result : this.__proto__;
+  return result !== 'false' ? result : this.__proto__;
 }
 function six(c) {
   this.__proto__ = c ? c : new caculater();
   var result = this.chain(6);
-  return result ? result : this.__proto__;
+  return result !== 'false' ? result : this.__proto__;
 }
 function seven(c) {
   this.__proto__ = c ? c : new caculater();
   var result = this.chain(7);
-  return result ? result : this.__proto__;
+  return result !== 'false' ? result : this.__proto__;
 }
 function eight(c) {
   this.__proto__ = c ? c : new caculater();
   var result = this.chain(8);
-  return result ? result : this.__proto__;
+  return result !== 'false' ? result : this.__proto__;
 }
 function nine(c) {
   this.__proto__ = c ? c : new caculater();
   var result = this.chain(9);
-  return result ? result : this.__proto__;
+  return result !== 'false' ? result : this.__proto__;
 }
 
 function plus(c) {
   this.__proto__ = c ? c : new caculater();
   var result = this.chain('+');
-  return result ? result : this.__proto__;
+  return result !== 'false' ? result : this.__proto__;
 }
 function minus(c) {
   this.__proto__ = c ? c : new caculater();
   var result = this.chain('-');
-  return result ? result : this.__proto__;
+  return result !== 'false' ? result : this.__proto__;
 }
 function times(c) {
   this.__proto__ = c ? c : new caculater();
   var result = this.chain('*');
-  return result ? result : this.__proto__;
+  return result !== 'false' ? result : this.__proto__;
 }
 function dividedBy(c) {
   this.__proto__ = c ? c : new caculater();
   var result = this.chain('/');
-  return result ? result : this.__proto__;
+  return result !== 'false' ? result : this.__proto__;
 }
 console.log(seven(times(five())));
-console.log(seven(times(five())));
+console.log(seven(times(zero())));
 console.log(four(plus(nine())));
 console.log(eight(minus(three())));
 console.log(six(dividedBy(two())));
