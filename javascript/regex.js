9bfba318eab1b6a4fafe7a6726d93a0c2fd2ca8f
ruanyl <ruanyu1@gmail.com>
2014-04-05 12:16:12 +0300

    pagination

--- javascript/regex.js
+++ javascript/regex.js
@@ -1,4 +1,6 @@
-var s = '/files/me/items/df9sufosjfosdfosdjfosdjf';
-
-var l = s.split('/').pop();
-console.log(l);
+function validPhoneNumber(phoneNumber){
+  var regexp = /\(\d{3}\)\S\d{3}-\d{4}/;
+  return regexp.test(phoneNumber);
+  //TODO: Return whether phoneNumber is in the proper form
+}
+console.log(validPhoneNumber("(123) 456-7890"));
